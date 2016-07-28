7a77c5435853d5711c4a05dd0834fa0d08acde3c
Chan Ming Wei <fateaverancus@gmail.com>
2016-07-28 16:27:55 +0800

    background

--- app/jobs/reservation_job.rb
+++ app/jobs/reservation_job.rb
@@ -4,7 +4,6 @@ class ReservationJob < ActiveJob::Base
   def perform(customer_id,host_id,reservation_id)
   	customer = User.find(customer_id)
   	host = User.find(host_id)
-  	ReservationMailer.booking_email( customer, host, reservation_id )
-    # Do something later
+  	ReservationMailer.booking_email( customer, host, reservation_id ).deliver_now
   end
 end
